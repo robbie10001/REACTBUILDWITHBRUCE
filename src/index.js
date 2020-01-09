52aabc637973546e73ba0e8435477cf324ba2408
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-10 10:13:27 +1100

    rect components and creating a class component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//REACT COMPONENTS 
 //Components are indepeent and resuable bits of code. The serve the same purpose as functions in plain Javascript,
 //however, they work in isolation and returns HTML via a render function. 
 
@@ -10,6 +11,14 @@ import ReactDOM from "react-dom";
 //With JSX, we can write expressions inside curly braces {}. 
 //the expression can be a react vairable, or property, or any other valid JavaScript expression.L0
 //JSX will execute the expression and return the result. 
+
+//CREATE  A CLASS COMPONENT 
+
+//When creating a React component, the component's name must start with an upper case letter. 
+//The component has to include the extend React.Component statement, 
+//this statement creates an inheritance to React.Component, and gives your compoent access to React.Components' functions.
+//The component also requires a render() method, this method returns HTML.
+
 
 const myelement = (
     <table>
