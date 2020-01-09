b750fd110607fee29e3dee0761795ef51a3b6594
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-09 18:03:08 +1100

    definition of state

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
-const App = function() {
-    const buttonText = "Click me";
-    return (
-    <div>
-        <div>
-            <section>
-                <label className ="label" htmlFor="name">Username:</label>
-            </section>
-                <input id="name" type="text" />
-            <section> 
-                <button>
-                    {buttonText}
-                </button> 
-            </section>
-        </div>
-        <div>
-            <section>
-                <lable className ="label" htmlFor="name">Email:</lable>
-            </section>
-            <section>
-                <input id="email" type="text" />
-            </section>
-            <section>
-                <button>
-                    {buttonText}
-                </button>
-            </section>
-        </div>
-            </div>
-    );
-};
+
+
+
+
+
+
+
 
 //PUT ALL OF THESE COMPONENTS WITHIN A SINGLE COMPONENT. 
 
