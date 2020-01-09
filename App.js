e30509d0d0da16aaf895bf586df4d663cbf89f05
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-09 18:04:46 +1100

    finished state syntax

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
+    state = { }; 
 //state is an object that can contain as many elements as we like.
-//we refer to the state objed anywhere in the component by using the following syntax.
+//we refer to the state object anywhere in the component by using the following syntax.
 //this.state.propertyname 
 //to change the value in the state object, we use the this.setState() method. 
 //when a value in the state object changes, the component will re-render, 
 //this means that the output will change according to the new values(s)
-    state = { }; 
 
 };
 
