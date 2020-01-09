f0afdb90d8c4ea23b933593677b87879c99e834e
robbie10001/rn <robbie_c1000@hotmail.com>
2020-01-10 09:21:46 +1100

    pseudocode for classes and import statements

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //react creates a virtual Document Object Model (DOM), which is a Application Programming Interface (API) for HTML and XML docs. 
+//React creates a virtual DOM in memory, where is does all the necessary manipulating, before making the changes in the browser DOM.
 
 
 class App extends React.Component {
+//a class is a type of function, but instead of using the keyword function to start it, we use the keyword class
+//and the properties are assigned inside a constructor() method. 
+
+
+
     state = { }; 
 //state is an object that can contain as many elements as we like.
 //we refer to the state object anywhere in the component by using the following syntax.
